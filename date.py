0f5915682ded2a618f86de1b7a7830a03e2aaf63
GSDV <sdvgab@gmail.com>
2025-03-13 20:28:20 -0500

    Getting rid of week ago

--- date.py
+++ date.py
@@ -7,7 +7,3 @@ def format_time(date):
 
 def time_now():
     return format_time(now)
-
-def time_one_week_ago():
-    seven_days_ago = now - timedelta(days=7)
-    return format_time(seven_days_ago)
