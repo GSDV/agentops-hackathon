169d861fb870c51c6558f154b5030625288967f3
GSDV <sdvgab@gmail.com>
2025-03-13 20:33:55 -0500

    spacing

--- agent_input.py
+++ agent_input.py
@@ -16,8 +16,9 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 AGENTOPS_API_KEY = os.getenv("AGENTOPS_API_KEY")
 agentops.init(AGENTOPS_API_KEY, default_tags=["chat_simulation"])
 
-instructions = f"You are an extremely financially-literate agent that is up to date ({time_now()}) with the Nasdaq and NYSE markets and their stocks."
 
+
+instructions = f"You are an extremely financially-literate agent that is up to date ({time_now()}) with the Nasdaq and NYSE markets and their stocks."
 
 async def run_web_agent(name, prompt):
     agent = Agent(
